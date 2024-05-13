56ddbade6de009d1274d1933fa6c6fac2194bb27
Francesco Pea <francesco.pea@student.univaq.it>
2024-05-13 22:02:06 +0200

    Rifinitura della classe Donation e correzione nomi degli attributi di book

--- engine/Donation.php
+++ engine/Donation.php
@@ -3,7 +3,7 @@ namespace Bibliotek\Entity;
 use DateTime;
 use Book;
 use User;
-
+use Exception;
 
 class Donation{
 
@@ -22,21 +22,29 @@ class Donation{
         $this->book = $_book;
         $this->giver = $_user;
         $this->quantity = $_quantity;
+        $this->presentationDate = new DateTime();
     }
     
-    public function DonationWithoutBook(string $_bookTitle, string $_bookISBN, int $_bookYear, string $_bookAuthor, string $_bookGenre, string $_bookDescription, User $_giver, int $_quantity){
+    public function DonationWithoutBook(string $_bookTitle, string $_bookISBN, int $_bookYear, string $_bookAuthor, string $_bookGenre, string $_bookDescription, int $_npages, User $_giver, int $_quantity){
         $this->book->title = $_bookTitle;
         $this->book->ISBN = $_bookISBN;
-        $this->book->year = $_bookYear;
-        $this->book->author = $_bookAuthor;
-        $this->book->genre = $_bookGenre;
+        $this->book->publishYear = $_bookYear;
+        $this->book->authors->array_push( $_bookAuthor);
+        $this->book->genres->array_push( $_bookGenre);
         $this->book->description = $_bookDescription;
+        $this->book->pagesNumber = $_npages;
         $this->quantity = $_quantity;
         $this->giver = $_giver;
+        $this->presentationDate = new DateTime();
     }
 
     public function setComment(string $_comment){
-        $this->comment = $_comment;
+        if($_comment->strlen <= 500){
+            $this->comment = $_comment;
+        }
+        else {
+            throw new Exception('Error: must be under 500 characters');
+        }
     }
 
     public function getComment() : string{
@@ -61,6 +69,46 @@ class Donation{
     }
 
     public function approve() {
+        $this->convalidationDate = new DateTime();
         $this->status = TRUE;
     }
+
+    public function decline() {
+        $this->convalidationDate = new DateTime();
+    }
+
+    public function  setBook(string $_bookTitle, string $_bookISBN, int $_bookYear, string $_bookAuthor, string $_bookGenre, string $_bookDescription, int $_npages){
+        $this->book->title = $_bookTitle;
+        $this->book->ISBN = $_bookISBN;
+        $this->book->publishYear = $_bookYear;
+        $this->book->authors->array_push( $_bookAuthor);
+        $this->book->genres->array_push( $_bookGenre);
+        $this->book->description = $_bookDescription;
+        $this->book->pagesNumber = $_npages;
+    }
+
+    public function getBook() :Book{
+        return $this->book;
+    }
+
+    public function setQuantity(int $_quantity){
+        $this->quantity = $_quantity;
+    }
+
+    public function getQuantity() :int {
+        return $this->quantity;
+    }
+
+    public function getGiver():User{
+        return $this->giver;
+    }
+
+    public function getPresentationDate():DateTime{
+        return $this->presentationDate;
+    }
+
+    public function getConvalidationDate():DateTime{
+        return $this->convalidationDate;
+    }
+
 }
