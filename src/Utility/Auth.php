d605c9599981b138ee257d3c0ca368dd31bdb2f8
Tommaso Tragno <tommaso.tragno@gmail.com>
2024-05-29 11:03:50 +0200

    added jwt configuration to composer, created and tested Utility\Auth.php for login and created middleware for login check

---
+++ src/Utility/Auth.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Bibliotek\Utility;
+
+use Bibliotek\Entity\User;
+use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
+
+class Auth {
+
+    private const SECRET_KEY = "U423cybUDGFVgjRdLnpj";
+    private const ISSUER = "bibliotek.tragno.cc";
+    private const AUDIENCE = "bibliotek";
+
+    public static function issueJWT($userId, $userRole = 'user') {
+        $now = time();
+        $payload = [
+            'iss' => self::ISSUER,
+            'aud' => self::AUDIENCE,
+            'iat' => $now,
+            'exp' => $now + 3600, // valid 1 hour
+            'sub' => $userId,
+            'role' => $userRole,
+        ];
+
+        return JWT::encode($payload, self::SECRET_KEY, 'HS256');
+    }
+
+    public static function isValidJWT($jwt) {
+        try {
+            JWT::decode($jwt, new Key(self::SECRET_KEY, 'HS256'));
+            return true;
+        } catch (\Exception $e) {
+            return false;
+        }
+    }
+
+    public static function getTokenPayload($jwt) {
+        try {
+            $decoded = JWT::decode($jwt, new Key(self::SECRET_KEY, 'HS256'));
+            return (array) $decoded;
+        } catch (\Exception $e) {
+            return false;
+        }
+    }
+
+    public static function currentUser() {
+        if (!isset($_COOKIE['jwt'])) {
+            return null;
+        }
+        $jwt = $_COOKIE['jwt'];
+        if (!self::isValidJWT($jwt)) {
+            return null;
+        }
+        $payload = self::getTokenPayload($jwt);
+        $user = $GLOBALS['entityManager']->find(User::class, $payload['sub']);
+        return $user;
+    }
+}
