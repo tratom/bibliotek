fa846eccb0025c84975ed00832f73c45d38ef281
Giovanni022 <168964655+Giovanni022@users.noreply.github.com>
2024-06-06 13:44:26 +0200

    reservation a book with methods to check availability and calculate the return date

--- src/Entity/Reservation.php
+++ src/Entity/Reservation.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Bibliotek\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
@@ -13,17 +14,17 @@ class Reservation {
     private int|null $id = null;
 
     #[ORM\Column(name: 'reservation_date', type: 'datetime')]
-    private string $reservationDate;
+    private \DateTime $reservationDate;
 
-    #[ORM\Column(name: 'estimated_date', type: 'date')]
-    private string $estimatedDate;
+    // #[ORM\Column(name: 'estimated_date', type: 'date')]
+    // private string $estimatedDate;
 
     /**
      * Bidirectional - Many Reservation are made by one User (OWNING SIDE)
      */
     #[ORM\ManyToOne(targetEntity: User::class, inversedBy: 'userReservations')]
     private User|null $user = null;
-    
+
     /**
      * Bidirectional - Many Reservation concerns one Book (OWNING SIDE)
      */
@@ -34,4 +35,62 @@ class Reservation {
     #[ORM\OneToOne(targetEntity: Loan::class)]
     #[ORM\JoinColumn(name: 'loan_id', referencedColumnName: 'id')]
     private Loan|null $loan = null;
+
+    // Getter and setter for id
+    public function getId(): int {
+        return $this->id;
+    }
+
+    // Getter and setter for reservationDate
+    public function getReservationDate(): \DateTime {
+        return $this->reservationDate;
+    }
+
+    public function setReservationDate(\DateTime $reservationDate): void {
+        $this->reservationDate = $reservationDate;
+    }
+
+    // Getter and setter for user
+    public function getUser(): User|null {
+        return $this->user;
+    }
+
+    public function setUser(User|null $user): void {
+        $this->user = $user;
+    }
+
+    // Getter and setter for book
+    public function getBook(): Book|null {
+        return $this->book;
+    }
+
+    public function setBook(Book|null $book): void {
+        $this->book = $book;
+    }
+
+    // Getter and setter for loan
+    public function getLoan(): Loan|null {
+        return $this->loan;
+    }
+
+    public function setLoan(Loan|null $loan): void {
+        $this->loan = $loan;
+    }
+
+    public function getMaxReturnDate(): \DateTime {
+        $start = clone $this->reservationDate;
+        $duration = $this->user->getMaxLoanDuration();
+        $start->modify("+$duration days");
+        return $start;
+    }
+
+    public function canBeLoaned(): bool {
+        $now = new \DateTime();
+        $start = $this->getBook()->getFirstAvailableReservationDate();
+        $end = $this->getBook()->getFirstAvailableReservationDate()->modify("+1 days");
+        // Check if today is between two dates, first available date for loan and first available date +1
+        return 
+            ($now->getTimestamp() >= $start->getTimestamp() && $now->getTimestamp() <= $end->getTimestamp())
+            && $this->getBook()->getAvailability() > 0;
+    }
 }
